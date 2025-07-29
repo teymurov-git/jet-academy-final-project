8f79f7fd5e8effd8b0bbb212437391d1f4e387ef
teymurov-git <teymurluyusif555@gmail.com>
2025-07-29 16:48:07 +0400

    updated

--- config/settings.py
+++ config/settings.py
@@ -153,3 +153,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'teymurluqaqas10@gmail.com'
 EMAIL_HOST_PASSWORD = 'sqbd iidr rqgq jfqx'
 EMAIL_PORT = 587
+
+
+# Celery settings
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
