8bb8d8d271bec8c9abefa5f23806af79534ef500
teymurov-git <teymurluyusif555@gmail.com>
2025-07-29 04:20:27 +0400

    updated

--- static/js/subscriber.js
+++ static/js/subscriber.js
@@ -1,23 +1,23 @@
-let subscriptionForm = document.getElementById('subscription-form')
+let subscriptionForm = document.getElementById('subscription-form');
 
 subscriptionForm.addEventListener('submit', function(event){
-    event.preventDefault()
-    let email = document.getElementById('email').value
+    event.preventDefault();
+
+    let email = document.getElementById('email').value;
+    let csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+
     fetch('http://127.0.0.1:8000/api/subscriber/', {
         method: 'POST',
         headers: {
             'Content-Type' : 'application/json',
-            'X-CSRFToken' : subscriptionForm.csrfmiddlewaretoken.value
+            'X-CSRFToken' : csrftoken
         },
         body: JSON.stringify({'email' : email})
-    }).then(
-        response => {
-            if (response.ok){
-                subscriptionForm.innerHTML = `<h2>Thanks for your subscription!</h2>`
-            }
-            else{
-                alert('Error')
-            }
+    }).then(response => {
+        if (response.ok){
+            subscriptionForm.innerHTML = `<h2>Thanks for your subscription!</h2>`;
+        } else {
+            alert('Error');
         }
-    )
-})
+    });
+});
